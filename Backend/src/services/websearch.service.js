acb51086c36aed07987b44df7f679a97958a35a1
Ritam Haldar <rickhaldar561@gmail.com>
2026-03-24 11:46:00 +0530

    feat: add web search service using Tavily API.

--- Backend/src/services/websearch.service.js
+++ Backend/src/services/websearch.service.js
@@ -4,9 +4,9 @@ const tvly = new tavily({
     apiKey: process.env.TAVILY_API_KEY
 })
 
-export async function webSearch(query) {
+export async function webSearch({ query }) {
     const response = await tvly.search(query, {
         max_results: 5
     })
-    return `Here are the search results go through them thoroughly and answer the question correctly: ${JSON.stringify(response.results)}`
+    return `Here are the search results go through them thoroughly and answer the question correctly also give priority to the dates the recent date should be given more priority: ${JSON.stringify(response.results)}`
 }
