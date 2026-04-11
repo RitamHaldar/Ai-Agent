e54cf0ff687f8f2ddae95d8c7384efc1e7c91c76
Ritam Haldar <rickhaldar561@gmail.com>
2026-04-11 21:35:09 +0530

    feat: implement user authentication system with email verification and JWT-based session management

--- Frontend/src/Features/Auth/Hooks/useAuth.jsx
+++ Frontend/src/Features/Auth/Hooks/useAuth.jsx
@@ -39,7 +39,7 @@ export const useAuth = () => {
 
         }
         catch (err) {
-            dispatch(setErr(err.response?.data?.message || 'Login failed. Please try again.'));
+            dispatch(setErr(null));
         }
         finally {
             dispatch(setLoading(false))
